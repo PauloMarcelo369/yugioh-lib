a157af0973bc04a3bab214a4d8a56468577caff4
Paulo Marcelo <marcelomp97447@gmail.com>
2024-09-26 20:32:24 -0300

    trocando o banco para sqlite por facilidade

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("deckMaster", "postgres", "paulo123", {
-  dialect: "postgres",
-  host: "localhost",
+const path = require("path");
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "../storage/database.sqlite"),
 });
 
 module.exports = sequelize;
