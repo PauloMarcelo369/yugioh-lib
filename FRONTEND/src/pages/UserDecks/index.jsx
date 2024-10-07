7d92c5ff41f0266e176cf1c35b4be953c490f3e2
Paulo Marcelo <marcelomp97447@gmail.com>
2024-10-07 15:58:29 -0300

    adicionado o update ao deck (crud finalizado)

--- FRONTEND/src/pages/UserDecks/index.jsx
+++ FRONTEND/src/pages/UserDecks/index.jsx
@@ -11,6 +11,7 @@ export const UserDecks = () => {
   const [form, setForm] = useState(false);
   const [error, setError] = useState("");
   const [decks, setDecks] = useState([]);
+  const [deckToEdit, setDeckToEdit] = useState(null);
 
   const getDecks = async () => {
     try {
@@ -41,7 +42,6 @@ export const UserDecks = () => {
         dataObj[name] = value;
       }
     }
-    console.log(dataObj);
     try {
       const response = await api.post("/deck", dataObj, {
         headers: { Authorization: `Bearer ${jwt}` },
@@ -56,6 +56,38 @@ export const UserDecks = () => {
       }
     } finally {
       setForm(false);
+    }
+  };
+
+  const updateDeckInfo = async (event) => {
+    event.preventDefault();
+    const form = document.getElementById("deckForm");
+    const formData = new FormData(form);
+    const dataObj = {};
+    for (const [name, value] of formData.entries()) {
+      dataObj[name] = name === "is_public" ? value === "true" : value;
+    }
+    try {
+      const response = await api.put(
+        `/deck/user_decks/${deckToEdit.id}`,
+        dataObj,
+        { headers: { Authorization: `Bearer ${jwt}` } }
+      );
+      const updatedDeck = response.data;
+      console.log(updatedDeck);
+      const newList = decks.map((deck) =>
+        deck.id === deckToEdit.id ? updatedDeck : deck
+      );
+      setDecks(newList);
+    } catch (error) {
+      if (isAxiosError(error)) {
+        setError(
+          `ocorreu um erro ao tentar salvar o deck: ${error.response.data.message}`
+        );
+      }
+    } finally {
+      setForm(false);
+      setDeckToEdit(null);
     }
   };
 
@@ -87,16 +119,25 @@ export const UserDecks = () => {
       </button>
       {form && (
         <div className={styles.formContainer}>
-          <form onSubmit={addDeck} id="deckForm">
+          <form onSubmit={deckToEdit ? updateDeckInfo : addDeck} id="deckForm">
             <div>
               <FaTimes
-                onClick={() => setForm(false)}
+                onClick={() => {
+                  setForm(false);
+                  setDeckToEdit(null);
+                }}
                 className="custom-icon fs-2 cursor-pointer"
               />
             </div>
             <div>
               <label htmlFor="deckName">deck name:</label>
-              <input type="text" id="deckName" name="name" required />
+              <input
+                type="text"
+                id="deckName"
+                name="name"
+                required
+                defaultValue={deckToEdit ? deckToEdit.name : ""}
+              />
             </div>
             <div>
               <label htmlFor="description">deck description:</label>
@@ -105,16 +146,24 @@ export const UserDecks = () => {
                 name="deck_description"
                 rows="4"
                 cols="50"
+                defaultValue={deckToEdit ? deckToEdit.deck_description : ""}
               ></textarea>
             </div>
             <div>
               <label htmlFor="visualization">deck visualization:</label>
-              <select id="visualization" name="is_public">
+              <select
+                id="visualization"
+                name="is_public"
+                defaultValue={deckToEdit ? deckToEdit.is_public : true}
+              >
                 <option value={true}>Public</option>
                 <option value={false}>Private</option>
               </select>
             </div>
-            <button className="btn btn-success">submit!</button>
+            <button className="btn btn-success">
+              {" "}
+              {deckToEdit ? "Update Deck" : "Submit!"}
+            </button>
           </form>
         </div>
       )}
@@ -134,6 +183,10 @@ export const UserDecks = () => {
                 <button
                   className="btn btn-success"
                   style={{ border: "none", boxShadow: "none" }}
+                  onClick={() => {
+                    setDeckToEdit(deck);
+                    setForm(true);
+                  }}
                 >
                   <FaEdit />
                 </button>
