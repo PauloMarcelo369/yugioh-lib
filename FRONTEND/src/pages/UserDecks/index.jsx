1dd4a1c5b45e2a002d09289f0160a0d4a5dfb422
Paulo Marcelo <marcelomp97447@gmail.com>
2024-10-07 13:51:28 -0300

    adicionando remoção de deck e formulario de cadastro de deck

--- FRONTEND/src/pages/UserDecks/index.jsx
+++ FRONTEND/src/pages/UserDecks/index.jsx
@@ -8,7 +8,7 @@ import { FaTimes, FaEdit } from "react-icons/fa";
 
 export const UserDecks = () => {
   const { jwt, username } = useAuth();
-  const [form, setForm] = useState("");
+  const [form, setForm] = useState(false);
   const [error, setError] = useState("");
   const [decks, setDecks] = useState([]);
 
@@ -29,6 +29,52 @@ export const UserDecks = () => {
     }
   };
 
+  const addDeck = async (event) => {
+    event.preventDefault();
+    const form = document.getElementById("deckForm");
+    const formData = new FormData(form);
+    const dataObj = {};
+    for (const [name, value] of formData.entries()) {
+      if (name === "is_public") {
+        dataObj[name] = value === "true";
+      } else {
+        dataObj[name] = value;
+      }
+    }
+    console.log(dataObj);
+    try {
+      const response = await api.post("/deck", dataObj, {
+        headers: { Authorization: `Bearer ${jwt}` },
+      });
+      const newDeck = response.data;
+      setDecks([...decks, newDeck]);
+    } catch (error) {
+      if (isAxiosError(error)) {
+        setError(
+          `ocorreu um erro ao tentar salvar o deck: ${error.response.data.message}`
+        );
+      }
+    } finally {
+      setForm(false);
+    }
+  };
+
+  const deleteDeck = async (id) => {
+    try {
+      await api.delete(`/deck/user_decks/${id}`, {
+        headers: { Authorization: `Bearer ${jwt}` },
+      });
+      const newDeckList = decks.filter((obj) => obj.id !== id);
+      setDecks(newDeckList);
+    } catch (error) {
+      if (isAxiosError(error)) {
+        setError(
+          `ocorreu um erro ao tentar deletar o deck: ${error.response.data.message}`
+        );
+      }
+    }
+  };
+
   useEffect(() => {
     getDecks();
   }, []);
@@ -36,11 +82,48 @@ export const UserDecks = () => {
   return (
     <div className={styles.pageContainer}>
       <h2>{username} decks:</h2>
+      <button className="btn btn-success" onClick={() => setForm(true)}>
+        New Deck!
+      </button>
+      {form && (
+        <div className={styles.formContainer}>
+          <form onSubmit={addDeck} id="deckForm">
+            <div>
+              <FaTimes
+                onClick={() => setForm(false)}
+                className="custom-icon fs-2 cursor-pointer"
+              />
+            </div>
+            <div>
+              <label htmlFor="deckName">deck name:</label>
+              <input type="text" id="deckName" name="name" required />
+            </div>
+            <div>
+              <label htmlFor="description">deck description:</label>
+              <textarea
+                id="description"
+                name="deck_description"
+                rows="4"
+                cols="50"
+              ></textarea>
+            </div>
+            <div>
+              <label htmlFor="visualization">deck visualization:</label>
+              <select id="visualization" name="is_public">
+                <option value={true}>Public</option>
+                <option value={false}>Private</option>
+              </select>
+            </div>
+            <button className="btn btn-success">submit!</button>
+          </form>
+        </div>
+      )}
+
       {error && <p className="text-danger">{error}</p>}
       <div className={styles.decksContainer}>
         {decks.map((deck) => {
           return (
-            <div className={styles.deck}>
+            <div className={styles.deck} key={deck.id}>
               <img src={DeckCase} alt="deckCase" />
               <div className={styles.deckInfo}>
                 <p>deck name: {deck.name}</p>
@@ -57,6 +140,7 @@ export const UserDecks = () => {
                 <button
                   className="btn btn-danger"
                   style={{ border: "none", boxShadow: "none" }}
+                  onClick={() => deleteDeck(deck.id)}
                 >
                   <FaTimes />
                 </button>
